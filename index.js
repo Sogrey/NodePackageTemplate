55ea2e0452ae486d7082255dbb658cefe88d6bb1
sogrey <suzhaoheng2006@163.com>
2021-08-21 11:26:37 +0800

    添加单元测试

--- index.js
+++ index.js
@@ -1,35 +1,6 @@
-// module.exports=require('./lib')
-
-function addZero(num) {
-    return num > 9 ? num : '0' + num
+var file = require('./lib/readAndWriteFile');
+ 
+module.exports = {
+    writeFile: file.writeFile,
+    readFile: file.readFile
 }
-
-let DateTimeFormatter = {
-    date: null,
-    init(date) {
-        try {
-            if (date) {
-                this.date = new Date(date)
-            }else{
-                this.date = new Date();
-            }
-        } catch (error) {
-            console.info(error)
-        }
-    },
-    getDateTime() {
-        let date = this.date || new Date()
-        let res = ''
-        res = date.getFullYear() + '-' + addZero(date.getMonth() + 1) + '-' + addZero(date.getDate()) +
-            ' ' + addZero(date.getHours()) + ':' + addZero(date.getMinutes()) + ':' + addZero(date.getSeconds())
-        return res
-    },
-    getDate() {
-        return this.getDateTime().split(' ')[0]
-    },
-    getTime() {
-        return this.getDateTime().split(' ')[1]
-    }
-}
-
-module.exports = DateTimeFormatter
